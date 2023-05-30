d01beef01379b5a7a1c94c3e6cb0c714d87014f3
HAlGhanim <imzaji98@gmail.com>
2023-05-30 23:35:20 +0300

    done with the first part pushing to finish at home

--- src/Components/Island.js
+++ src/Components/Island.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default function Island({ island }) {
+export default function Island({ island, setIsland }) {
+  const handleChange = () => {
+    setIsland(island);
+  };
   return (
-    <div className="Island">
+    <div className="Island" onClick={handleChange}>
       <h3>{island.name}</h3>
       {/* YOU HAVE TO CHANGE THE VISITORS COUNT */}
       <p>Visitors 0</p>
--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 export default function IslandForm({ island }) {
+  const [fName, setfName] = useState("");
+
+  const nameValue = (event) => {
+    setfName(event.target.value);
+  };
   return (
     <div className="form">
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
-      <button className="book" onClick={() => {}}>
+      <input id="fullName" type="text" placeholder="Type Full Name" />
+      <input
+        id="phoneNumber"
+        type="tel"
+        placeholder="Type Phone Number"
+        // value={fName}
+      />
+      <button
+        className="book"
+        // onClick={() => {
+        //   if (
+        //     window.confirm(
+        //       `Are you sure you want to book to ${island.name} with the Name: ${}, phone: ${}`
+        //     )
+        //   ) {
+        //   }
+        // }}
+      >
         Book for today!
       </button>
     </div>
