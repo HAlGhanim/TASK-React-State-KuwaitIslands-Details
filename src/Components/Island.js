28ed9de43954192a5ba1cf337748a68d1ea175fb
HAlGhanim <imzaji98@gmail.com>
2023-05-31 14:33:27 +0300

    Done with the task not sure if I did the visitors count correctly. The number only changes after I click on another island.

--- src/Components/Island.js
+++ src/Components/Island.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Island({ island, setIsland }) {
   const handleChange = () => {
     setIsland(island);
@@ -8,7 +6,7 @@ export default function Island({ island, setIsland }) {
     <div className="Island" onClick={handleChange}>
       <h3>{island.name}</h3>
       {/* YOU HAVE TO CHANGE THE VISITORS COUNT */}
-      <p>Visitors 0</p>
+      <p>Visitors: {island.visitors}</p>
       <img src={island.img} alt={island.name} />
     </div>
   );
--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -2,33 +2,37 @@ import { useState } from "react";
 
 export default function IslandForm({ island }) {
   const [fName, setfName] = useState("");
+  const [pNumber, setpNumber] = useState("");
 
-  const nameValue = (event) => {
-    setfName(event.target.value);
+  const handleClick = () => {
+    if (
+      window.confirm(
+        `Are you sure you want to book to ${island.name} with the Name: ${fName}, phone: ${pNumber}`
+      )
+    ) {
+      island.visitors++;
+      setfName("");
+      setpNumber("");
+    }
   };
   return (
     <div className="form">
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input id="fullName" type="text" placeholder="Type Full Name" />
       <input
-        id="phoneNumber"
+        value={fName}
+        placeholder="Type Full Name"
+        onChange={(event) => setfName(event.target.value)}
+      />
+      <input
+        value={pNumber}
         type="tel"
         placeholder="Type Phone Number"
-        // value={fName}
+        pattern="[0-9]{4}-[0-9]{4}"
+        onChange={(event) => setpNumber(event.target.value)}
       />
-      <button
-        className="book"
-        // onClick={() => {
-        //   if (
-        //     window.confirm(
-        //       `Are you sure you want to book to ${island.name} with the Name: ${}, phone: ${}`
-        //     )
-        //   ) {
-        //   }
-        // }}
-      >
+      <button className="book" onClick={handleClick}>
         Book for today!
       </button>
     </div>
